2b4afda1c908429f8437e58d41167b84945f9827
juliolikescoding <jalfaro@techlaunch.io>
2019-02-27 20:51:00 -0500

    first commit

---
+++ .history/src/index_20190227203058.js
@@ -0,0 +1,72 @@
+import React,{Component} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import files from './files.js';
+import moment from 'moment';
+
+// function Row({file}){ 
+//     const timeString = moment(file.updated_at).fromNow();
+//     return(
+//         <div className='row'>
+//             <i className='fa fa-folder folder-icon'/>
+//             <span className='file-name'>{file.name}</span>
+//             <span className='file-description'>{file.latestCommit.message}</span>
+//             <span className='time'>{timeString}</span>
+//         </div>
+//     ); 
+// };
+
+function randomId() {
+    return Math.random().toString(36).replace(/[^a-z]+/g, '').substr(2, 10);
+};
+
+function Icon({type}){
+    if(type.type == 'folder'){
+        return(
+            <i className='fa fa-folder folder-icon'/>
+        );
+    }else if(type.type == 'file'){
+        return(
+            <i className='fa fa-file folder-icon'/>
+        );
+    };
+};
+
+function Name({name}){
+    return(
+        <span className='file-name'>{name.name}</span>
+    );
+};
+
+function Message({message}){
+    return(
+        <span className='file-description'>{message.latestCommit.message}</span>
+    );
+};
+
+function Time({time}){
+    const timeString = moment(time.updated_at).fromNow();
+    return(
+        <span className='time'>{timeString}</span>
+    );
+};
+
+class FileStructure extends Component{
+    render(){
+        console.log(files);
+        return(
+            this.props.files.map(file=>(
+                <div className='row' key={randomId()}>
+                    <Icon type={file}/>
+                    <Name name={file} />
+                    <Message message={file}/>
+                    <Time time={file} />
+                </div>
+            ))
+        );
+    };
+};
+
+
+ReactDOM.render(<FileStructure files={files}/>, document.getElementById('root'));
+
